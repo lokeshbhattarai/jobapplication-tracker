28360ecab544f4c7292a4c0f613e54ff7b5936de
Suraj Thapaliya <opensource.suraj@gmail.com>
2016-07-12 16:13:22 -0500

    Controller Dashbaord and ApplyJob

---
+++ src/main/java/mum/waa/jobapplicationtracker/controller/ApplyJobController.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mum.waa.jobapplicationtracker.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author suraj
+ */
+@Controller
+public class ApplyJobController {
+    
+    
+    @RequestMapping("/applyjob")
+    public String loadDashboard(){        
+        return "applyjob";
+    }
+    
+}
--- src/main/webapp/WEB-INF/jsp/includes/footer.jsp
+++ src/main/webapp/WEB-INF/jsp/includes/footer.jsp
@@ -21,6 +21,11 @@
 
         <script src="<c:url value='/resources/js/bootstrap-datepicker.js'></c:url>"></script>
         <script src="<c:url value='/resoruces/js/bootstrap-table.js'></c:url>"></script>
+        
+        <script src="<c:url value='/resoruces/js/angular.min.js'></c:url>"></script>
+        <script src="<c:url value='/resoruces/js/controller/registrationController.js'></c:url>"></script>
+        
+        
         <script src="<c:url value='/resources/js/script.js'></c:url>"></script>	
 </body>
 
