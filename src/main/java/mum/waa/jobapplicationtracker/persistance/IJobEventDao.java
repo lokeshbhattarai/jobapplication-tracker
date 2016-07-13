1e1b73d198823506dc8878fb85ba375e81e271d5
Lokesh <bhattarai.lokesh@gmail.com>
2016-07-13 00:59:38 -0500

    service and persistant for jobevent

--- src/main/java/mum/waa/jobapplicationtracker/persistance/IJobEventDao.java
+++ src/main/java/mum/waa/jobapplicationtracker/persistance/IJobEventDao.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package mum.waa.jobapplicationtracker.service;
+package mum.waa.jobapplicationtracker.persistance;
 
 import java.util.List;
 import mum.waa.jobapplicationtracker.model.JobEvent;
@@ -12,13 +12,9 @@ import mum.waa.jobapplicationtracker.model.JobEvent;
  *
  * @author lokex
  */
-public interface IJobEventService {
-    
-    List<JobEvent> getAllJobEvents(long userId);
-    
-    List<JobEvent> getJobEventsByJob(long userId,long jobOpeningId);
-    
-    void addJobEvent(long userId,long jobOpeningId,JobEvent jobEvent);
-    
+public interface IJobEventDao {
     JobEvent getById(long Id);
+    void addJobEvent(JobEvent jobEvent);
+    List<JobEvent> getAllJobEvents(long userId);
+    List<JobEvent> getJobEventsByJob(long userId,long jobOpeningId);
 }
