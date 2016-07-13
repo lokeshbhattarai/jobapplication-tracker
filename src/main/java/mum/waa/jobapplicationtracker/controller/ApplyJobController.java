b9ba24cb5b551d51aa22a183cec657728345fe21
suraj <shrestha.sj.npl@gmail.com>
2016-07-13 00:02:31 -0500

    adding applied job search

--- src/main/java/mum/waa/jobapplicationtracker/controller/ApplyJobController.java
+++ src/main/java/mum/waa/jobapplicationtracker/controller/ApplyJobController.java
@@ -5,8 +5,14 @@
  */
 package mum.waa.jobapplicationtracker.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+import mum.waa.jobapplicationtracker.model.JobOpening;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  *
@@ -15,10 +21,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ApplyJobController {
     
-    
     @RequestMapping("/applyjob")
     public String loadDashboard(){        
         return "applyjob";
     }
     
+    @RequestMapping(value = "/applyjoblist", method = RequestMethod.GET)
+    public String loadAppliedJobListPage(){
+        return "appliedjobsearch";
+    }
+    
+    @RequestMapping(value = "/getappliedjoblist/{itemsPerPage}/{pageno}", method = RequestMethod.GET)
+    public @ResponseBody List<JobOpening> getAppliedJobList(@PathVariable int itemsPerPage, @PathVariable int pageno){
+        List<JobOpening> jobs = new ArrayList<>();
+        for (int i = 0; i < itemsPerPage; i++) {
+            JobOpening job = new JobOpening();
+            job.setJobTitle("C# developer");
+            job.setCompanyName("Microsoft Tech");
+            jobs.add(job);
+        }
+        return jobs;
+    }
 }
