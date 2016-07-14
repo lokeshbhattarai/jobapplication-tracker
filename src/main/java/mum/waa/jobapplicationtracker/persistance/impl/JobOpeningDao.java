b7cbc3fb3f417035b0c854e8d04f05fdf3b7ef20
Suraj Thapaliya <opensource.suraj@gmail.com>
2016-07-13 21:03:19 -0500

    Need to change

--- src/main/java/mum/waa/jobapplicationtracker/persistance/impl/JobOpeningDao.java
+++ src/main/java/mum/waa/jobapplicationtracker/persistance/impl/JobOpeningDao.java
@@ -8,10 +8,13 @@ package mum.waa.jobapplicationtracker.persistance.impl;
 import java.text.SimpleDateFormat;
 import java.util.List;
 import mum.waa.jobapplicationtracker.model.JobOpening;
+import mum.waa.jobapplicationtracker.model.User;
 import mum.waa.jobapplicationtracker.persistance.AbstractDao;
 import mum.waa.jobapplicationtracker.persistance.IJobOpeningDao;
 import org.hibernate.Criteria;
 import org.hibernate.Query;
+import org.hibernate.SQLQuery;
+import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
 import org.joda.time.LocalDate;
 import org.springframework.stereotype.Repository;
@@ -21,40 +24,57 @@ import org.springframework.stereotype.Repository;
  * @author lokex
  */
 @Repository
-public class JobOpeningDao extends AbstractDao<Long, JobOpening> implements IJobOpeningDao{
+public class JobOpeningDao extends AbstractDao<Long, JobOpening> implements IJobOpeningDao {
 
     @Override
-    public void addJobOpening( JobOpening jobOpening) {
+    public void addJobOpening(JobOpening jobOpening) {
         persist(jobOpening);
     }
 
     @Override
     public List<JobOpening> getAllJobOpenings(long userId) {
-         Criteria criteria = createEntityCriteria();
-         criteria.add(Restrictions.eq("user_id", userId));
-         return (List<JobOpening>) criteria.list();
+//        try {
+        //String sql = "FROM job_openings where user_id = '3'";
+        
+        User user = new User();
+        user.setId((long)3);
+          Criteria criteria = createEntityCriteria();
+          criteria.add(Restrictions.eq("user", user));
+        return (List<JobOpening>) criteria.list();
+        
+        
+//        Session s = this.getSession();
+//        Query query = s.createQuery("from job_openings where user_id = '3' ");
+//        List list = query.list();
+//        return list;
+//        
+//      
+//        }catch(Exception e) {
+//            System.out.println(e.getMessage());
+//        }
+//       return null;
     }
 
     @Override
     public void logJobAppy(long jobOpeningId) {
-        
+
         LocalDate now = LocalDate.now();
-        
+
         SimpleDateFormat df = new SimpleDateFormat("dd/MM/yyyy");
         String appliedDate = df.format(now);
-        
+
         Query query = getSession().createSQLQuery("update job_openings set applied_date=:date where id=:ID");
         query.setString("date", appliedDate);
         query.setString("ID", String.valueOf(jobOpeningId));
         query.executeUpdate();
-        
+
     }
 
     @Override
     public JobOpening getById(long jobOpeningId) {
-         Criteria criteria = createEntityCriteria();
-         criteria.add(Restrictions.eq("id", jobOpeningId));
-         return (JobOpening) criteria.uniqueResult();
+        Criteria criteria = createEntityCriteria();
+        criteria.add(Restrictions.eq("id", jobOpeningId));
+        return (JobOpening) criteria.uniqueResult();
     }
-    
+
 }
